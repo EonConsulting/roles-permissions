a6b2c9c7e991a795bea8be9b23fe9726c87a1623
Peace Ngara <peacengara@aol.com>
2017-04-11 09:49:16 +0200

    Updates

--- src/Routes/web.php
+++ src/Routes/web.php
@@ -6,7 +6,7 @@
  * Time: 5:31 PM
  */
 
-Route::group(['middleware' => ['web', 'auth'], 'prefix' => '/admin', 'namespace' => 'EONConsulting\\RolesPermissions\\Http\\Controllers\\'], function() {
+Route::group(['middleware' => ['web', 'auth', 'instructor'], 'prefix' => '/admin', 'namespace' => 'EONConsulting\\RolesPermissions\\Http\\Controllers\\'], function() {
     Route::group(['namespace' => 'Admin\\Roles\\'], function() {
         Route::get('/groups', ['as' => 'eon.admin.groups', 'uses' => 'GroupsController@index']);
         Route::get('/groups/create', ['as' => 'eon.admin.groups.create', 'uses' => 'GroupsController@create']);
